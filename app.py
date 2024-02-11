b028d6dbe04d6ac6b10b135d61984d57109a161f
IvonaSav <savovskaivona1@gmail.com>
2024-02-11 20:28:36 +0100

    Second Endpoint without telegram

--- app.py
+++ app.py
@@ -52,16 +52,61 @@ def total_spending_by_age(user_id):
     else:
          return jsonify({'error': 'User not found'}), 404
 
+
+@app.route('/total_spent/<int:user_id>', methods=['GET'])
+def total_spent(user_id):
+    age_ranges = {
+        '18-24': (18, 24),
+        '25-30': (25, 30),
+        '31-36': (31, 36),
+        '37-47': (37, 47),
+        '>47': (48, float('inf')),
+    }
+
+    db_path = 'C:\\Users\\User\\Desktop\\users_vouchers.db'
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT age FROM user_info WHERE user_id = ?', (user_id,))
+    age_result = cursor.fetchone()
+
+    cursor.close()
+    connection.close()
+
+    if age_result:
+        age = age_result[0]
+
+        found_range = None
+
+        for range_name, age_range in age_ranges.items():
+            if age_range[0] <= age <= age_range[1]:
+                found_range = range_name
+                break
+
+        if found_range is not None:
+            connection = sqlite3.connect(db_path)
+            cursor = connection.cursor()
+
+            cursor.execute('''
+                SELECT AVG(us.money_spent) as average_spending
+                FROM user_info as u
+                JOIN user_spending as us ON u.user_id = us.user_id
+                WHERE u.age >= ? AND u.age <= ?
+            ''', age_range)
+
+
+            average_spending = cursor.fetchone()[0]
+
+            cursor.close()
+            connection.close()
+
+
+            return jsonify({'average_spending': average_spending})
+        else:
+            return jsonify({'error': 'No matching age range found'}), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-# @app.route('/total_spent/<int:user_id>', methods=['GET'])
-# def total_spent(user_id):
-#     return f'Welcome to the Flask API! User ID: {user_id}'
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 # @app.route('/write_to_mongodb', methods=['POST'])
 # def write_to_mongodb():
   
